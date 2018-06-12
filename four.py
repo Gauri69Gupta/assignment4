1819b4091914492fe3f9ea9e95dc90fc0f485747
Gauri69Gupta <gauri.gupta166@gmail.com>
2018-06-13 01:23:10 +0530

    second change

---
+++ four.py
@@ -0,0 +1,40 @@
+print("enter the elements of list")
+a=input("enter first elememt")
+b=input("enter second elememt")
+c=input("enter third elememt")
+d=input("enter fourth elememt")
+list=[a,b,c,d]
+print(list)
+list2=['google','apple','facebook','microsoft','tesla']
+list2=list+list2
+print(list2)
+**********************
+Question-4
+list=[2,3,5,4,1,6,7,8,4,0,7,8,9]
+list.sort()
+print(list)
+***********************
+Question-5
+list1=[1,2,4,3]
+list1.sort()
+list2=[5,9,7,6]
+list2.sort()
+list3=list1+list2
+print(list3)
+***********************
+Question-6
+stack=['Gauri','Riya','Meenal']
+stack.append('anshul')
+stack.append('giya')
+print(stack)
+stack.pop()
+print(stack)
+queue=['a','b','c']
+queue.append('mili')
+queue.append('giya')
+print(queue)
+queue.pop(0)
+print(queue)
+queue.pop(0)
+print(queue)
+end of assignment
